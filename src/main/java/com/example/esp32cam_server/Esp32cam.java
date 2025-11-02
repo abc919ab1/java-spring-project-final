5d98d5a6d34d6e8a91d5ca1791c739ee6fca89e4
Oskar <abc919ab1@gmail.com>
2025-11-02 15:13:59 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/example/esp32cam_server/Esp32cam.java
@@ -0,0 +1,13 @@
+package com.example.esp32cam_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esp32cam {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esp32cam.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esp32cam_server/Esp32camServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esp32cam_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esp32camServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
