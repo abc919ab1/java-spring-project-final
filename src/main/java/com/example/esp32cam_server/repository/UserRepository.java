84b4d5fca60b97219cd7816781afcdc2bb32bb3f
Oskar <abc919ab1@gmail.com>
2025-11-04 07:25:34 +0100

    UserRepository done for now.

--- src/main/java/com/example/esp32cam_server/repository/UserRepository.java
+++ src/main/java/com/example/esp32cam_server/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.esp32cam_server.repository;
 
-public class UserRepository {
+import com.example.esp32cam_server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
